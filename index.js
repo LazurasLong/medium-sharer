9421427944212ff77d12d23643be2f108f0450e1
dantehemerson <dantehemerson@gmail.com>
2018-10-02 20:04:33 -0500

    Set toolbar position #

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 (function() {
-	
+	let Toolbar = document.getElementById('Toolbar')
+
+	document.addEventListener('mouseup', function() {
+		let texto = window.getSelection().toString()
+		let startOffset = window.getSelection().getRangeAt(0).startOffset
+		let endOffset = window.getSelection().getRangeAt(0).endOffset
+
+		let range = window.getSelection().getRangeAt(0)
+		let boundary = range.getBoundingClientRect()
+
+		if(!boundary || ((boundary.height === 0 && boundary.width === 0) && range.startContainer === range.endContainer)) {
+			boundary = range.startContainer.getBoundingClientRect()			
+		}
+
+		setToolbarPosition(boundary.left + boundary.width / 2, boundary.top)
+
+	})
+
+	function setToolbarPosition(x, y) {
+		Toolbar.style.left = `${x}px`
+		Toolbar.style.top = `${y}px`
+	}
+
+
+
+
+
+
 })()
